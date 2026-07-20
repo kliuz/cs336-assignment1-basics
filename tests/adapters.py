86bab109a8f30c10545652ed9fc4373cd7b963e4
kliuz <33242952+kliuz@users.noreply.github.com>
2026-07-19 17:47:30 -0700

    Rename bpe.py to train_bpe.py to be better match the assignment

--- tests/adapters.py
+++ tests/adapters.py
@@ -9,7 +9,7 @@ import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
-from cs336_basics import bpe
+from cs336_basics import train_bpe
 
 
 def run_linear(
@@ -591,4 +591,4 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    return bpe.train_bpe(input_path, vocab_size, special_tokens)
+    return train_bpe.train_bpe(input_path, vocab_size, special_tokens)
